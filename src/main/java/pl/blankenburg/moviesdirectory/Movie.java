a87fef0f877589b3cfd5b8103e729e5702acad29
mcblankenburg <maciej.blankenburg@gmail.com>
2022-05-11 22:29:50 +0200

    configurated Movie.class model to work with hibernate

--- src/main/java/pl/blankenburg/moviesdirectory/Movie.java
+++ src/main/java/pl/blankenburg/moviesdirectory/Movie.java
@@ -3,12 +3,18 @@ package pl.blankenburg.moviesdirectory;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.util.UUID;
 
 @Getter
 @Setter
+@Entity
 public class Movie {
+
+    @Id
     private UUID id;
+
     private String name;
 
 
